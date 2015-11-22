c010290f5fb8b10904edcaaaa2246eb4b74a019e
Ben Kellogg <benk691@gmail.com>
2015-11-22 14:12:17 -0700

    Created funtion to subtract a full day of product, but has not been tested. Helps resolve issue #22.

--- HerbalifeInventory/HerbalifeInventory.py
+++ HerbalifeInventory/HerbalifeInventory.py
@@ -27,9 +27,37 @@ def readInventory():
 			inventoryTable.update({product : subTable})
 	return inventoryTable
 
+def writeInventory(inventoryTable):
+	pass
+
+def calculateDaysLeft(inventoryTable):
+	for product in inventoryTable:
+		quantity = inventoryTable[product][quantityKey]
+		qPerM = inventoryTable[product][quantityPerMealKey]
+		mPerD = inventoryTable[product][mealPerDayKey]
+		if quantity == 0 or qPerM == 0 or mPerD == 0:
+			# This will result in 0 or divide by 0 error so default it to 0
+			inventoryTable[product][daysLeftkey] = Decimal("0.00")
+			continue
+		inventoryTable[product][daysLeftkey] = quantity / (qPerM * mPerD)
+
+def dailyUsage(inventoryTable):
+	'''
+	Takes off one day of use for every product
+	'''
+	for product in inventoryTable:
+		quantity = inventoryTable[product][quantityKey]
+		qPerM = inventoryTable[product][quantityPerMealKey]
+		mPerD = inventoryTable[product][mealPerDayKey]
+		if quantity > Decimal("0.00"):
+			inventoryTable[product][quantityKey] = quantity - (qPerM * mPerD)
+			if inventoryTable[product][quantityKey] < Decimal("0.00"):
+				inventoryTable[product][quantityKey] = Decimal("0.00")
+
 def main():
 	decimal.setcontext(invContext)
 	inventoryTable = readInventory()
+	calculateDaysLeft(inventoryTable)
 	print inventoryTable
 	return 0
 

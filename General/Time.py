0f131597797a8e5e7c5f075834b1f616ac06a250
Ben Kellogg <benk691@gmail.com>
2016-05-21 10:16:43 -0600

    Added in a common function to create the datetime and added in compare operators.

--- General/Time.py
+++ General/Time.py
@@ -7,8 +7,6 @@ class Time:
 		'''
 		@param timestamp - string in the format of %Y-%m-%d_%H:%M:%S
 		'''
-		self.timeStruct = None
-		self.epoch = None
 		self.dateTime = None
 		self._readTimestamp(timestamp)
 
@@ -50,9 +48,7 @@ class Time:
 			timestamp = '{0}-{1}-{2}_{3}:{4}:{5}'.format(self.dateTime.year + years, self.dateTime.month + months, 
 				self.dateTime.day, self.dateTime.hour, self.dateTime.minute, self.dateTime.second)
 
-		tmpTimeStruct = time.strptime(timestamp, Time.pattern)
-		tmpEpoch = time.mktime(tmpTimeStruct)
-		tmpDateTime = datetime.datetime.fromtimestamp(tmpEpoch)
+		tmpDateTime = self._constructDateTime(timestamp)
 
 		diff = tmpDateTime - self.dateTime
 
@@ -61,21 +57,46 @@ class Time:
 
 		self.applyTimeDelta(weeks=weeks, days=days, hours=hours, minutes=minutes, seconds=seconds)
 		
-
 	def _readTimestamp(self, timestamp):
 		'''
 		Reads in a timestamp that is in the format %Y-%m-%d_%H:%M:%S, and converts
 		it into an epoch time and a time struct
 		@param timestamp - string with a date in the format of %Y-%m-%d_%H:%M:%S
 		'''
-		self.timeStruct = time.strptime(timestamp, Time.pattern)
-		self.epoch = time.mktime(self.timeStruct)
-		self.dateTime = datetime.datetime.fromtimestamp(self.epoch)
+		self.dateTime = self._constructDateTime(timestamp)
+
+	def _constructDateTime(self, timestamp):
+		'''
+		Constructs a datetime object from a timestamp
+		@param timestamp - string with a date in the format of %Y-%m-%d_%H:%M:%S
+		@return datetime corresponding to the timestamp
+		'''
+		timeStruct = time.strptime(timestamp, Time.pattern)
+		epoch = time.mktime(timeStruct)
+		return datetime.datetime.fromtimestamp(epoch)
 
 	def _toStr(self):
+		'''
+		Converts Time class into a string in the format of %Y-%m-%d_%H:%M:%S
+		'''
 		return '%d-%02d-%02d_%02d:%02d:%02d' % (self.dateTime.year, self.dateTime.month, 
 			self.dateTime.day, self.dateTime.hour, self.dateTime.minute, self.dateTime.second)
 
+	def __eq__(self, rhs):
+		return self.dateTime == rhs.dateTime
+
+	def __lt__(self, rhs):
+		return self.dateTime < rhs.dateTime
+
+	def __le__(self, rhs):
+		return self.dateTime <= rhs.dateTime
+
+	def __gt__(self, rhs):
+		return self.dateTime > rhs.dateTime
+
+	def __ge__(self, rhs):
+		return self.dateTime >= rhs.dateTime
+	
 	def __repr__(self):
 		return self._toStr()
 
